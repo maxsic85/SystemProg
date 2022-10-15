936da2e6f30172d663aa6d4a53e783be3d3b894b
Maksim.S <maxsic85@mail.ru>
2022-10-16 00:15:47 +0500

    task01

---
+++ SystemProg/Assets/Code/HomeWork02/JobStruct.cs
@@ -0,0 +1,21 @@
+﻿using Unity.Collections;
+using Unity.Jobs;
+
+
+
+namespace HomeWork02
+{
+
+    public struct JobStruct : IJob
+    {
+        public NativeArray<int> array;
+        public void Execute()
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > 10) array[i] = 0;
+            }
+        }
+
+    }
+}
---
+++ SystemProg/Assets/Code/HomeWork02/TestJob.cs
@@ -0,0 +1,42 @@
+﻿using Unity.Collections;
+using Unity.Jobs;
+using UnityEngine;
+
+
+
+namespace HomeWork02
+{
+    public class TestJob:MonoBehaviour 
+    {
+        private const int Length = 10;
+
+        void Start()
+        {
+            NativeArray<int> array = new NativeArray<int>(Length, Allocator.TempJob);
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = Random.Range(0, 100);
+                Debug.Log($"input {array[i]}");
+            }
+
+            JobStruct job = new JobStruct();
+            job.array = array;
+          
+            JobHandle handle = job.Schedule();
+            handle.Complete();
+
+            if (handle.IsCompleted)
+            {
+                for (int i = 0; i < job.array.Length; i++)
+                {
+                    Debug.Log($"output: {job.array[i]}");
+                }
+              
+            }
+                  
+            array.Dispose();
+
+        }
+    }
+
+}
